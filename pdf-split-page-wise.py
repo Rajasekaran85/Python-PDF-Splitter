f14cdb3dbe8d4dffb1f5e6b99628be62d8918c74
Rajasekaran A <rajasekaran.al@gmail.com>
2022-06-15 17:25:57 +0530

    Update pdf-split-page-wise.py
    # using fitz library for creating the split pdf
    # using pikepdf library for applying the file creation datetime & fast web view
    
    Simplified version

--- pdf-split-page-wise.py
+++ pdf-split-page-wise.py
@@ -2,7 +2,6 @@ import os
 import fitz
 from pikepdf import Pdf
 from datetime import datetime
-import glob
 
 
 print("\n PDF Splitter - Single Page wise PDF \n")
@@ -34,7 +33,7 @@ for rem in os.listdir(Out):
 		os.remove(Out + rem)
 
 
-# This loop convert the combined pdf into page level pdf 
+# Loop convert the combined pdf into page level pdf 
 print("\n Extracting the Pages ... \n")
 for fname in os.listdir(filepath):
 	if not fname.endswith(".pdf"):
@@ -42,25 +41,20 @@ for fname in os.listdir(filepath):
 	path = os.path.join(filepath, fname)
 	pdfname = os.path.splitext(fname)[0]
 	input_file = fitz.open(path)
-	output_file = Out + "Extract-" + pdfname
 	for n, page in enumerate(input_file, start=0):  # n = for each page number
 		new_pdf = fitz.open()
 		new_pdf.insert_pdf(input_file, from_page=n, to_page=n)
-		new_pdf.save(output_file + "_"+ f"{n+1:03}" + '.pdf')                   # saving the extracted pdf file (this is not final file)
-
-
-
-# This loop assign the pdf creation datetime & fastweb view for the page level pdf
-print("\n Applying Fast Web View ... \n")
-for ext in glob.glob(Out + "*.pdf"):
-	with Pdf.open(ext) as pdf:
-		rem = "Extract-"
-		final = os.path.join(ext, ext.replace(rem, ""))
-		with pdf.open_metadata(set_pikepdf_as_editor=False) as meta:         # set_pikepdf_as_editor=False for applying the Producer value
-			meta["pdf:Producer"] = "pikepdf"
-			meta["xmp:CreateDate"] = datetime.now(datetime.utcnow().astimezone().tzinfo).isoformat()   # Current date of file saving date
-			meta["xmp:ModifyDate"] = datetime.now(datetime.utcnow().astimezone().tzinfo).isoformat()
-		pdf.save(final, linearize=True)
-		pdf.close()
-		print(final)
-	os.remove(ext)                                            # delete the intermediate extracted pdf file
+		output_file = Out + "Extract-" + pdfname + "_" + f"{n+1:03}" + '.pdf'
+		new_pdf.save(output_file)                   # saving the extracted pdf file (this is not final file)
+		new_pdf.close()
+		with Pdf.open(output_file) as pdf:           # assign the pdf creation datetime & fastweb view for the page level pdf
+			rem = "Extract-"
+			final =  output_file.replace(rem, "")
+			with pdf.open_metadata(set_pikepdf_as_editor=False) as meta:         # set_pikepdf_as_editor=False for applying the Producer value
+				meta["pdf:Producer"] = "pikepdf"
+				meta["xmp:CreateDate"] = datetime.now(datetime.utcnow().astimezone().tzinfo).isoformat()   # Current date of file saving date
+				meta["xmp:ModifyDate"] = datetime.now(datetime.utcnow().astimezone().tzinfo).isoformat()
+			pdf.save(final, linearize=True)
+			pdf.close()
+			print(final)
+		os.remove(output_file)                                            # delete the intermediate extracted pdf file
